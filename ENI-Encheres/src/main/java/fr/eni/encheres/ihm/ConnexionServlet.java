07df6ce44422a4735e1d0147b57b98519b5b3cce
blemee2021 <blemee2021@26SE22-76N1422.ad.campus-eni.fr>
2022-06-08 17:02:29 +0200

    resolution conflit

--- ENI-Encheres/src/main/java/fr/eni/encheres/ihm/ConnexionServlet.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/ihm/ConnexionServlet.java
@@ -6,9 +6,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import fr.eni.encheres.bll.BLLException;
 import fr.eni.encheres.bll.UtilisateurManager;
 import fr.eni.encheres.bo.Utilisateur;
+
 
 /**
  * Servlet implementation class ConnexionServlet

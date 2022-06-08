c448cfbee8dcc170695ec7781fd50a22a5c3a098
BrewenEni <107037222+BrewenEni@users.noreply.github.com>
2022-06-08 12:17:29 +0200

    Feature/1009 deconnecter (#3)
    * fichiers de connexion
    
    * ajout servlet deconnexion
    
    Co-authored-by: blemee2021 <blemee2021@26SE22-76N1422.ad.campus-eni.fr>

---
+++ ENI-Encheres/fr/eni/encheres/bll/BLLException.java
@@ -0,0 +1,14 @@
+package fr.eni.suiviRepas.bll;
+
+public class BLLException extends Exception {
+
+	public BLLException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BLLException(Exception e) {
+		super(e);
+	}
+
+	
+}

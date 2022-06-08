15e5d37ac0e4127ada19334412241dc0573da5d9
blemee2021 <blemee2021@26SE22-76N1422.ad.campus-eni.fr>
2022-06-08 16:29:42 +0200

    connexion utilisateur

--- ENI-Encheres/src/main/java/fr/eni/encheres/ihm/ConnexionServlet.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/ihm/ConnexionServlet.java
@@ -7,19 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.encheres.bll.BLLException;
+import fr.eni.encheres.bll.UtilisateurManager;
+import fr.eni.encheres.bo.Utilisateur;
+
+
 /**
  * Servlet implementation class ConnexionServlet
  */
 @WebServlet("/connexion")
 public class ConnexionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private UtilisateurManager UtilisateurMger;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ConnexionServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    	UtilisateurMger = new UtilisateurManager();
     }
 
 	/**
@@ -34,9 +39,29 @@ public class ConnexionServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String login = req.getParameter("identifiant");
+		String pwd = req.getParameter("motDePasse");
+		
+			Utilisateur utilisateur = null;
+			try {
+				utilisateur = UtilisateurMger.verifierConnexion(login, pwd);
+				req.getSession(true).setAttribute("utilisateur", utilisateur);
+				resp.sendRedirect(req.getContextPath() + "/accueil");
+			} catch (BLLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(utilisateur == null) {
+				//TODO gerer mdp invalide
+			}
+			
+//			req.setAttribute("identifiant", login);
+//			req.setAttribute("motDePasse", pwd);
+//			req.setAttribute("erreur", "Connexion refusée. L'identifiant ou le mot de passe est invalide.");
+//			req.getRequestDispatcher("/WEB-INF/pages/connexion.jsp").forward(req, resp);
 	}
 
 }

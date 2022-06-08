c7b943fdaab182a9d5fbffa3a1346b625f0b0a2b
BrewenEni <107037222+BrewenEni@users.noreply.github.com>
2022-06-08 16:52:45 +0200

    Feature/1009 deconnecter (#7)
    * fichiers de connexion
    
    * ajout servlet deconnexion
    
    * recuperation données utilisateur à la connexion
    
    Co-authored-by: blemee2021 <blemee2021@26SE22-76N1422.ad.campus-eni.fr>

---
+++ ENI-Encheres/src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
@@ -0,0 +1,25 @@
+package fr.eni.encheres.bll;
+
+import java.sql.SQLException;
+
+import fr.eni.encheres.bo.Utilisateur;
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.UtilisateurDAO;
+
+public class UtilisateurManager {
+
+	private UtilisateurDAO utilisateurDAO;
+	
+	public UtilisateurManager() {
+		utilisateurDAO = DAOFactory.getDAOUtilisateur();
+	}
+	public Utilisateur verifierConnexion(String login, String pw) throws BLLException {
+		Utilisateur utilisateur = null;
+		try {
+			utilisateur = utilisateurDAO.verifierConnexion(login, pw);
+		} catch (SQLException e) {
+			throw new BLLException(e);
+		}
+		return utilisateur;
+	}
+}

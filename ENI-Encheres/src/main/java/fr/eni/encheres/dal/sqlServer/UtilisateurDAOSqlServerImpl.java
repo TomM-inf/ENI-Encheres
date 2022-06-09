3519c6ff13f32530934a029e4ab66c51a93f7a7d
BrewenEni <107037222+BrewenEni@users.noreply.github.com>
2022-06-09 16:27:26 +0200

    Feature/1001 connecter (#12)
    * fichiers de connexion
    
    * ajout servlet deconnexion
    
    * deplacement
    
    * fichier connexion
    
    * connexion utilisateur
    
    * Feature/1009 deconnecter (#7)
    
    * fichiers de connexion
    
    * ajout servlet deconnexion
    
    * recuperation données utilisateur à la connexion
    
    Co-authored-by: blemee2021 <blemee2021@26SE22-76N1422.ad.campus-eni.fr>
    
    * ajout chemin accueil
    
    * resolution conflit
    
    * resolution conflit
    
    * renvoi l'utilisateur à la connexion et verifie la connexion
    
    * .
    
    * .
    
    * gerer mot de passe ou identifiant incorrect
    
    * relier le bouton connexion à la page connexion
    
    * correction connexion
    
    Co-authored-by: blemee2021 <blemee2021@26SE22-76N1422.ad.campus-eni.fr>

--- ENI-Encheres/src/main/java/fr/eni/encheres/dal/sqlServer/UtilisateurDAOSqlServerImpl.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/dal/sqlServer/UtilisateurDAOSqlServerImpl.java
@@ -11,7 +11,8 @@ import fr.eni.encheres.dal.UtilisateurDAO;
 
 public class UtilisateurDAOSqlServerImpl implements UtilisateurDAO {
 
-	private static final String CONNEXION = "SELECT * FROM UTILISATEURS WHERE ?=? AND mot_de_passe=?";
+	private static final String CONNEXION_PSEUDO = "SELECT * FROM UTILISATEURS WHERE pseudo=? AND mot_de_passe=?";
+	private static final String CONNEXION_EMAIL = "SELECT * FROM UTILISATEURS WHERE email=? AND mot_de_passe=?";
 
 	@Override
 	public Utilisateur verifierConnexion(String login, String pw) throws SQLException {
@@ -20,62 +21,50 @@ public class UtilisateurDAOSqlServerImpl implements UtilisateurDAO {
 		try {
 			conn = ConnectionProvider.getConnection();
 			conn.setAutoCommit(false);
-
-			PreparedStatement stmt = conn.prepareStatement(CONNEXION);
-			stmt.setString(1, "pseudo");
-			stmt.setString(2, login);
-			stmt.setString(3, pw);
+			PreparedStatement stmt = conn.prepareStatement(CONNEXION_PSEUDO);
+			stmt.setString(1, login);
+			stmt.setString(2, pw);
 			ResultSet rs = stmt.executeQuery();
-			while (rs.next()) {
-				if (rs.getString(1) == null) {
-					return null;
-				} else {
-					utilisateur = new Utilisateur();
-					utilisateur.setPseudo(rs.getString("pseudo"));
-					utilisateur.setNom(rs.getString("nom"));
-					utilisateur.setPrenom(rs.getString("prenom"));
-					utilisateur.setEmail(rs.getString("email"));
-					utilisateur.setTelephone(rs.getString("telephone"));
-					utilisateur.setRue(rs.getString("rue"));
-					utilisateur.setCodePostal(rs.getString("code_postal"));
-					utilisateur.setVille(rs.getString("ville"));
-					utilisateur.setMotDePasse(rs.getString("mot_de_passe"));
-					utilisateur.setCredit(rs.getInt("credit"));
-					utilisateur.setAdministrateur(rs.getBoolean("administrateur"));
-				}
+			if (rs.next()) {
+				utilisateur = new Utilisateur();
+				utilisateur.setPseudo(rs.getString("pseudo"));
+				utilisateur.setNom(rs.getString("nom"));
+				utilisateur.setPrenom(rs.getString("prenom"));
+				utilisateur.setEmail(rs.getString("email"));
+				utilisateur.setTelephone(rs.getString("telephone"));
+				utilisateur.setRue(rs.getString("rue"));
+				utilisateur.setCodePostal(rs.getString("code_postal"));
+				utilisateur.setVille(rs.getString("ville"));
+				utilisateur.setMotDePasse(rs.getString("mot_de_passe"));
+				utilisateur.setCredit(rs.getInt("credit"));
+				utilisateur.setAdministrateur(rs.getBoolean("administrateur"));
+			} else {
+				utilisateur = null;
 			}
 			if (utilisateur != null) {
-			
 				return utilisateur;
 			} else {
-				PreparedStatement statement = conn.prepareStatement(CONNEXION);
-				statement.setString(1, "email");
-				statement.setString(2, login);
-				statement.setString(3, pw);
+				PreparedStatement statement = conn.prepareStatement(CONNEXION_EMAIL);
+				statement.setString(1, login);
+				statement.setString(2, pw);
 				ResultSet res = statement.executeQuery();
-				while (res.next()) {
-					if (rs.getString(1) == null) {
-						return null;
-					} else {
-						utilisateur = new Utilisateur();
-						utilisateur.setPseudo(res.getString("pseudo"));
-						utilisateur.setNom(res.getString("nom"));
-						utilisateur.setPrenom(res.getString("prenom"));
-						utilisateur.setEmail(res.getString("email"));
-						utilisateur.setTelephone(res.getString("telephone"));
-						utilisateur.setRue(res.getString("rue"));
-						utilisateur.setCodePostal(res.getString("code_postal"));
-						utilisateur.setVille(res.getString("ville"));
-						utilisateur.setMotDePasse(res.getString("mot_de_passe"));
-						utilisateur.setCredit(res.getInt("credit"));
-						utilisateur.setAdministrateur(res.getBoolean("administrateur"));
-					}
+				if (res.next()) {
+					utilisateur = new Utilisateur();
+					utilisateur.setPseudo(res.getString("pseudo"));
+					utilisateur.setNom(res.getString("nom"));
+					utilisateur.setPrenom(res.getString("prenom"));
+					utilisateur.setEmail(res.getString("email"));
+					utilisateur.setTelephone(res.getString("telephone"));
+					utilisateur.setRue(res.getString("rue"));
+					utilisateur.setCodePostal(res.getString("code_postal"));
+					utilisateur.setVille(res.getString("ville"));
+					utilisateur.setMotDePasse(res.getString("mot_de_passe"));
+					utilisateur.setCredit(res.getInt("credit"));
+					utilisateur.setAdministrateur(res.getBoolean("administrateur"));
+				} else {
+					utilisateur = null;
 				}
-				if(utilisateur != null) {
-					return utilisateur;
-				}else {
-					return null;
-				}
+				return utilisateur;
 			}
 		} catch (SQLException e) {
 

f55448c75a0e33545472100040f2d59299b86b6b
BrewenEni <brewen.lemee2021@campus-eni.fr>
2022-06-13 14:19:37 +0200

    suite affichage profil

---
+++ ENI-Encheres/bin/src/main/webapp/WEB-INF/pages/connexion.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>titre</title>
+<link rel="stylesheet"
+	href="${pageContext.request.contextPath}/css/main.css" />
+</head>
+<body>
+	<div class="connexion">
+	<div class="form-connexion">
+		<c:if test="${not empty erreur }">
+			<h3 style="color: red;">${erreur}</h3>
+		</c:if>
+		<form action="${pageContext.request.contextPath}/connexion"
+			method="post">
+			<div class="connexion-gauche">
+				<label>Identifiant : </label> <label>Mot de passe : </label>
+				<button>Connexion</button>
+			</div>
+			<div class="connexion-droite">
+				<input type="text" name="identifiant"
+					value="${not empty identifiant ? identifiant : ""}" /><br /> <input
+					type="password" name="motDePasse" id="" /><br /> <input
+					type="checkbox" name="chkBox"> <label>Se souvenir
+					de moi</label><br />
+				<!-- url mot de passe oublié -->
+				<a href="${url}">Mot de passe oublié</a>
+			</div>
+		</form>
+
+	</div>
+	<br />
+	<input type="button" name="btnCreerCompte" value="Créer un compte">
+	</div>
+</body>
+</html>
--- ENI-Encheres/src/main/java/fr/eni/encheres/ihm/AfficherProfilServlet.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/ihm/AfficherProfilServlet.java
@@ -24,22 +24,35 @@ public class AfficherProfilServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//TODO recuperer les infos user
 		String pseudo = req.getParameter("pseudo");
+		String idEnchere = req.getParameter("idEnchere");
 		Utilisateur utilisateur = null;
 		try {
 			//TODO Gérer quel affichage en fonction des boolean (simple, enchereRemporte,monProfil)
+			pseudo = "tdb";
 			utilisateur = UtilisateurMger.getUtilisateurByPseudo(pseudo);
-			req.getSession(true).setAttribute("utilisateur", utilisateur);
+			req.setAttribute("utilisateur", utilisateur);
+			req.setAttribute("simple", false);
+			req.setAttribute("enchereRemporte", false);
+			req.setAttribute("monProfil", false);
+			
+			if(pseudo.equals(((Utilisateur) req.getSession().getAttribute("utilisateur")).getPseudo())) {
+				req.setAttribute("monProfil", true);
+//			}else if(){
+//				req.setAttribute("enchereRemporte", true);
+			}
+			else if(req.getSession().getAttribute("utilisateur") != null) {
+				req.setAttribute("simple", true);
+			}
+			
 			req.getRequestDispatcher("/WEB-INF/pages/profil.jsp").forward(req, resp);
 		} catch (BLLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Envoyer vers modifier profil
 		super.doPost(req, resp);
 	}
 }

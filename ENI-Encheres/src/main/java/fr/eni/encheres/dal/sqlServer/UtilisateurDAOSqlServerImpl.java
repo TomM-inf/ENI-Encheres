3d8c189e0099ce9102513fa63e3a9fe8899d7712
BrewenEni <brewen.lemee2021@campus-eni.fr>
2022-06-14 16:31:35 +0200

    correction salt

--- ENI-Encheres/src/main/java/fr/eni/encheres/dal/sqlServer/UtilisateurDAOSqlServerImpl.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/dal/sqlServer/UtilisateurDAOSqlServerImpl.java
@@ -362,31 +362,6 @@ public class UtilisateurDAOSqlServerImpl implements UtilisateurDAO {
 		return password;
 	}
 
-  
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            md.update(salt.getBytes());
-            byte[] bytes = md.digest(passwordToHash.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bytes.length; i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
-                        .substring(1));
-            }
-            generatedPassword = sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return generatedPassword;
-    }
-	
-    // Add salt
-    private static String getSalt() throws NoSuchAlgorithmException {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-        return salt.toString();
-    }
-
 	@Override
 	public boolean isAlphaNumeric(String s) {
         return s != null && s.matches("^[a-zA-Z0-9]*$");
